a143a2c7899912109f30570ed834a1e081efdc8f
Jonathan Dung <jonathandung@yahoo.com>
2026-09-10 17:21:19 +0800

    commit

--- RoguePlanet.cpp
+++ RoguePlanet.cpp
@@ -1,11 +1,4 @@
 int main() {
-    printf("Do'nt be evil, i will help you\nJust be calm and your health first, if you do'nt like Microsoft anymore, move to other softwares\n");
-    printf("There was tons of softwares and vendors that will keep your health and not strict, like Cisco\n");
-    printf("Do'nt harm users please, while you are frustrated, not only microsoft exist, there was tons of orgs");
-    printf("i give you one pro tip, report to CISA what you find in the future, for pressure microsoft while staying legal\n");
-    printf("Do'nt do the july threat, stay legal, stay a security researcher do'nt cross the line to be a criminal\n");
-    printf("If you find another vuln again in microsoft defender or bitlocker, escalate to CISA legal and ethical and the same pressure the public\n");
-    printf("You want protect the public, ok, do move from iressponsible disclosure to escalation to CISA, for protect users and everything stay safe\n");
-    printf("Ok, you understanded, we are friends in the cybersecurity community\n");
-    printf("Remember, sleep and protect your health first, otherwise you will risk yourself\n");
+    printf("shut up microsoft glazer\n");
+    return 1;
 }
